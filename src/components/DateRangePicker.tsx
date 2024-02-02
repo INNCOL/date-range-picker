858bc44d420bac8865c4d62182f56bace89c22ca
Inncol <contacto@inncol.com.mx>
2024-02-02 17:04:11 -0600

    feat(Dev): server for testing porpuse

---
+++ src/components/DateRangePicker.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DateRangePicker: React.FC = () => {
+    return <div>Hola</div>
+}
+
+export default DateRangePicker
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+// src/index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DateRangePicker from './components/DateRangePicker';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <DateRangePicker />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
